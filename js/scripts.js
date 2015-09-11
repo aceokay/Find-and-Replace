d6bbca42e1e64c1d0c9105f0c616becb001ef0bc
Austin <aokincaid@gmail.com>
2015-09-11 11:23:15 -0700

    Added spec and functionality to handle simple punctuation in main string

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
 var findReplace = function(sentence, remove, replace) {
+  sentence = sentence.toLowerCase();
+  remove = remove.toLowerCase();
+  replace = replace.toLowerCase();
+
   var sentenceArray = sentence.split(" ");
 
-  for(var word_index = 0; word_index < sentenceArray.length; word_index++){
-    if (sentenceArray[word_index] === remove) {
-      sentenceArray[word_index] = replace;
+  for (var word_index = 0; word_index < sentenceArray.length; word_index++){
+    var punctuationLocation = sentenceArray[word_index].match(/[.,!?;:]/g);
+    if (punctuationLocation === null) {
+      if (sentenceArray[word_index] === remove) {
+        sentenceArray[word_index] = replace;
+      }
+    } else {
+      var comparison = sentenceArray[word_index];
+      if (comparison.replace(/[.,!?;:]/g, "") === remove) {
+        sentenceArray[word_index] = replace.concat(punctuationLocation);
+      }
     }
   };
 
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,12 @@ describe("findReplace", function() {
   it("will handle a large text entries", function() {
     expect(findReplace("i ate cheese eight times as a champ of the ate life-style", "ate", "can't eat")).to.equal("i can't eat cheese eight times as a champ of the can't eat life-style");
   });
+
+  it("will handle words with capitalization issues within inputs through normalization", function() {
+    expect(findReplace("FootBall Season Is Upon uS and I love footBalL", "fOOTball", "SHOUTING")).to.equal("shouting season is upon us and i love shouting");
+  });
+
+  it("will handle a sentence with simple punctuation and still find removal words and replace them", function() {
+    expect(findReplace("I think real chimps, as animals, are the best chimps. chimps? Chimps!", "chimps", "lava-rocks")).to.equal("i think real lava-rocks, as animals, are the best lava-rocks. lava-rocks? lava-rocks!");
+  });
 });
