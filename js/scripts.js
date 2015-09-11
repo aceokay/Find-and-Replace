c4009fd9f296900077ca73fff81d62fb0af86078
Austin <aokincaid@gmail.com>
2015-09-11 09:49:22 -0700

    Add spec and method to perform basic functionality of replacing items within a given string

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 var findReplace = function(sentence, remove, replace) {
-  
+  var sentenceArray = sentence.split(" ");
+
+  for(var word_index = 0; word_index < sentenceArray.length; word_index++){
+    if (sentenceArray[word_index] === remove) {
+      sentenceArray[word_index] = replace;
+    }
+  };
+
+  return sentenceArray.join(" ");
 };
